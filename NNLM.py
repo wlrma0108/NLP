c200d3c7d057dcddff6db5cc5c7c4e46b2c34bc8
wlrma0108 <dlwlrma0108@gmail.com>
2022-04-02 15:45:26 +0900

    commit

--- NNLM.py
+++ NNLM.py
@@ -63,7 +63,7 @@ if __name__=='__main__':
         
         loss=criterion(output,target_batch)
         if (epoch+1)%1000==0:
-            print('Epoch:','%04d'%(epoch+1),'cost=','{:.5f}'.format(loss))
+            print('Epoch:','%04d'%(epoch+1),'cost=','{:.6f}'.format(loss))
             loss.backward()
             optimizer.step()
             
